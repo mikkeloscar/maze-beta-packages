162310258c9a202f38a943444e72d2a2f8d33fcf
Mikkel Oscar Lyderik Larsen <m@moscar.net>
2017-08-28 00:24:58 +0200

    Inspect env

--- build.sh
+++ build.sh
@@ -6,15 +6,17 @@ REPO="https://beta.maze-ci.org/mikkeloscar/maze"
 # if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
     aur_pkgs=$(ruby -ryaml -e 'puts ARGV[1..-1].inject(YAML.load(File.read(ARGV[0]))) {|acc, key| acc[key] }' packages.yml aur)
 
+    ls -la
+    pwd
     mkdir -p build
     chown 1000:1000 -R build 
 
-    for pkg in $aur_pkgs; do
-        docker run --net=host --rm -it -v "$(pwd)/build:/build" -w "/build" \
-            --user $UGNAME:$UGNAME mikkeloscar/maze-build-travis:latest \
-            --repo $REPO \
-            --origin aur \
-            --package $pkg \
-            --upload
-    done
+    # for pkg in $aur_pkgs; do
+    #     docker run --net=host --rm -it -v "$(pwd)/build:/build" -w "/build" \
+    #         --user $UGNAME:$UGNAME mikkeloscar/maze-build-travis:latest \
+    #         --repo $REPO \
+    #         --origin aur \
+    #         --package $pkg \
+    #         --upload
+    # done
 # fi

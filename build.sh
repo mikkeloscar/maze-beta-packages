1d8567573bca1caf47ddfa222a86c8428fac60f8
Mikkel Oscar Lyderik Larsen <m@moscar.net>
2017-08-28 00:33:29 +0200

    Clenup build

--- build.sh
+++ build.sh
@@ -3,17 +3,15 @@
 UGNAME="builder"
 REPO="https://beta.maze-ci.org/mikkeloscar/maze"
 
+# setup build dir
+mkdir -p build
+sudo chown 1000:1000 -R build 
+
 # if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
     aur_pkgs=$(ruby -ryaml -e 'puts ARGV[1..-1].inject(YAML.load(File.read(ARGV[0]))) {|acc, key| acc[key] }' packages.yml aur)
 
-    ls -la
-    pwd
-    mkdir -p build
-    sudo chown 1000:1000 -R build 
-    ls -la
-    pwd
-
     for pkg in $aur_pkgs; do
+        echo "=== $pkg ==="
         docker run --net=host --rm -it -v "$(pwd)/build:/build" -w "/build" \
             --user $UGNAME:$UGNAME mikkeloscar/maze-build-travis:latest \
             --repo $REPO \
